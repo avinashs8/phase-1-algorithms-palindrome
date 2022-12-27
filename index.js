799ccb64ec941cd7aa1f1827f1ca86f62f580c2e
avinash singh <avinash8.as@gmail.com>
2022-12-26 21:36:02 -0500

    commit tree 7eb73e95115d183ee7a3022f5d03d42bc0b424a5

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let reverse = ""
+  for (let i= word.length - 1; i >=0; i--){
+    reverse += word[i]
+    }
+    if (reverse === word){
+        return true
+      } else{
+        return false
+      }
+    }
+  
+
 
 /* 
   Add your pseudocode here
